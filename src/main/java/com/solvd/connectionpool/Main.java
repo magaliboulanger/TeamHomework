afa420b0ee5f761c7f0891fda41855cdfcdfc604
Josh Corino <joshuc98@gmail.com>
2020-12-04 00:28:25 -0300

    Changes demo

--- src/main/java/com/solvd/connectionpool/Main.java
+++ src/main/java/com/solvd/connectionpool/Main.java
@@ -14,18 +14,7 @@ public class Main {
 	public static void main(String[] args ) {
 		ExecutorService ex=Executors.newFixedThreadPool(10);
 		MyConnectionPool pool = MyConnectionPool.getInstance();
-		Test1 t11 =new Test1(pool);
-		Test2 t21 =new Test2(pool);
-		Test1 t12 =new Test1(pool);
-		Test2 t22 =new Test2(pool);
-		Test1 t13 =new Test1(pool);
-		Test2 t23 =new Test2(pool);
-		ex.execute(t11);
-		ex.execute(t12);
-		ex.execute(t13);
-		ex.execute(t21);
-		ex.execute(t22);
-		ex.execute(t23);
+
 
 		ex.shutdown();
 		try {
